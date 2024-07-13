ac3188fa09b3019a6cbc30a50c76ef5b5d9045c2
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-13 18:18:06 +0530

    Module Wrapper

--- iife.js
+++ iife.js
@@ -1,15 +1,22 @@
 //Immediate Invoked Function Expression(IIFE pattern)
-//In this,each function has its private scope
+//In thiseach function has its private scope
 //so there is no conflict of variables naming
 //Node.js using this IIFE pattern to get rid of naming conflicts of variable and functions
 //In Node.js, we can have the same function name and variables in modules due to IIFE
+//each Module function has 5 parameters wrapped  "exports,modules,require,__filename,__dirname"
 
-(function () {
+// (function(exports,modules,require,__filename,__dirname){
+//   const superHero="Batman"
+//   console.log(superHero)
+//    })();
+
+
+(function (message) {
   const superHero = "Batman";
-  console.log(superHero);
-})();
+  console.log(message,superHero);
+})("Hello");
 
-(function () {
+(function (message) {
   const superHero = "Superman";
-  console.log(superHero);
-})();
+  console.log(message,superHero);
+})("Hey");
