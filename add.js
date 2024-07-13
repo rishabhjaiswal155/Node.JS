4f13886cd796f2cce24b36cbae74951116b5a8bf
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-13 16:53:25 +0530

    Module exports using module.exports object

--- add.js
+++ add.js
@@ -1,6 +1,7 @@
 const add=(a,b)=>{
 return a+b
 }
+module.exports=add
 
-const sum=add(2,3)
-console.log(sum)
+// const sum=add(2,3)
+// console.log(sum)
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-require("./add")
-console.log("Hello Node from index.js")
+const addFn = require("./add");
+console.log("Hello Node from index.js");
+const sum = addFn(2, 3);
+console.log(sum);
+const sum1 = addFn(10, 5);
+console.log(sum1);
